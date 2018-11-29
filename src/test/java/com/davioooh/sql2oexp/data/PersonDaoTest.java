6f353d6d87c36ac52c42a040534aa126230755f0
davioooh <davidcastelletti@yahoo.it>
2018-11-29 17:07:25 +0100

    Update tests

--- src/test/java/com/davioooh/sql2oexp/data/PersonDaoTest.java
+++ src/test/java/com/davioooh/sql2oexp/data/PersonDaoTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
@@ -29,35 +31,6 @@ class PersonDaoTest {
         personDao = new PersonDao(dataSource);
     }
 
-    @Test
-    void shouldReturn5Persons() {
-        List<Person> personList = personDao.findAll();
-        assertEquals(5, personList.size());
-    }
-
-    @Test
-    void shouldReturnMario() {
-        Optional<Person> mario = personDao.findById(1);
-        assertTrue(mario.isPresent());
-        mario.ifPresent(p -> {
-            assertEquals("Mario", p.getName());
-            assertEquals(40, p.getAge());
-        });
-
-    }
-
-    @Test
-    void shouldReturnOptionalEmpty() {
-        Optional<Person> empty = personDao.findById(10);
-        assertFalse(empty.isPresent());
-    }
-
-    @Test
-    void shouldCount5Persons() {
-        int count = personDao.countAll();
-        assertEquals(5, count);
-    }
-
     Person newPerson() {
         Person p = new Person();
         p.setName("Diego");
@@ -65,25 +38,79 @@ class PersonDaoTest {
         return p;
     }
 
+    List<Person> newPersons() {
+        Person p1 = new Person();
+        p1.setName("Carlo");
+        p1.setAge(40);
+
+        Person p2 = new Person();
+        p2.setName("Paola");
+        p2.setAge(30);
+
+        return Arrays.asList(p1, p2);
+    }
+
+    @Test
+    void shouldReturn5Persons() {
+        List<Person> personList = personDao.findAll();
+
+        assertThat(personList).hasSize(5);
+    }
+
+    @Test
+    void shouldReturnMario() {
+        Optional<Person> mario = personDao.findById(1);
+
+        assertTrue(mario.isPresent());
+        mario.ifPresent(p -> {
+            assertThat(p.getName()).isEqualTo("Mario");
+            assertThat(p.getAge()).isEqualTo(40);
+        });
+    }
+
+    @Test
+    void shouldReturnOptionalEmpty() {
+        Optional<Person> empty = personDao.findById(10);
+
+        assertFalse(empty.isPresent());
+    }
+
+    @Test
+    void shouldCount5Persons() {
+        int count = personDao.countAll();
+
+        assertThat(count).isEqualTo(5);
+    }
+
     @Test
     void shouldInsertDiego() {
         Person person = personDao.insert(newPerson());
+
         assertTrue(person.getId() > 0);
-        assertEquals("Diego", person.getName());
-        assertEquals(15, person.getAge());
+        assertThat(person.getName()).isEqualTo("Diego");
+        assertThat(person.getAge()).isEqualTo(15);
+    }
+
+    @Test
+    void shouldInsertAll() {
+        List<Person> personList = personDao.insertAll(newPersons());
+
+        assertThat(personList).hasSize(2);
+        assertThat(personList).allMatch(p -> p.getId() > 0);
     }
 
     @Test
     void shouldUpdateClaudia() {
-        Person p = personDao.findById(5)
+        Person person = personDao.findById(5)
                 .orElseThrow(() -> new IllegalStateException("Claudia non trovata..."));
 
-        assertEquals("Claudia", p.getName());
-        assertEquals(60, p.getAge());
+        assertThat(person.getName()).isEqualTo("Claudia");
+        assertThat(person.getAge()).isEqualTo(60);
 
-        p.setAge(50);
-        Person updated = personDao.update(p);
-        assertEquals(50, updated.getAge());
+        person.setAge(50);
+        Person updated = personDao.update(person);
+
+        assertThat(updated.getAge()).isEqualTo(50);
     }
 
     @Test
